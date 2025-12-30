4e8d18945caade77c1ec8042e71bd4b60ecf9dee
Nam Nguyen <nguyenvietnam2401@gmail.com>
2025-12-30 21:59:03 +1100

    build: improve block.sh with better array syntax

--- scripts/block.sh
+++ scripts/block.sh
@@ -17,22 +17,12 @@ shift
 targets=( "$@" )
 
 if (( ${#targets[@]} == 0 )); then
-  targets=()
-  while read -r file; do
-    targets+=( "$file" )
-  done < <(find "$SRC_PATH" -type f -name '*.html')
+  targets=( $(find "$SRC_PATH" -type f -name '*.html') )
 fi
 
 for target in "${targets[@]}"; do
-  start_line_numbers=()
-  while read -r line_number; do
-    start_line_numbers+=("$line_number")
-  done < <(sed -n "/<!-- block-start: $block -->/=" "$target")
-
-  end_line_numbers=()
-  while read -r line_number; do
-    end_line_numbers+=("$line_number")
-  done < <(sed -n "/<!-- block-end: $block -->/=" "$target")
+  start_line_numbers=( $(sed -n "/<!-- block-start: $block -->/=" "$target") )
+  end_line_numbers=( $(sed -n "/<!-- block-end: $block -->/=" "$target") )
 
   if [[ "${#start_line_numbers[@]}" -ne "${#end_line_numbers[@]}" ]]; then
     echo "$target: '$block' mismatched number of start/end block tags"
@@ -40,15 +30,8 @@ for target in "${targets[@]}"; do
   fi
 
   for index in "${!start_line_numbers[@]}"; do
-    start_line_numbers=()
-    while read -r line_number; do
-      start_line_numbers+=("$line_number")
-    done < <(sed -n "/<!-- block-start: $block -->/=" "$target")
-
-    end_line_numbers=()
-    while read -r line_number; do
-      end_line_numbers+=("$line_number")
-    done < <(sed -n "/<!-- block-end: $block -->/=" "$target")
+    start_line_numbers=( $(sed -n "/<!-- block-start: $block -->/=" "$target") )
+    end_line_numbers=( $(sed -n "/<!-- block-end: $block -->/=" "$target") )
 
     if [[ "${#start_line_numbers[@]}" -ne "${#end_line_numbers[@]}" ]]; then
       echo "$target: '$block' mismatched number of start/end block tags"
